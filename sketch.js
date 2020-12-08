585d015ee3a38dda852b4fec47e9cc9b4e598776
sonali.sadhukhan88@gmail.com <sonali.sadhukhan88@gmail.com>
2020-12-08 09:16:56 +0530

    Added platform object in sketch.js

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new Box(100,200,400,10);
 
 }
 
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
